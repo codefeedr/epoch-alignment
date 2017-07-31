32e4c4d3a9d3f650c67752d52a1cdda8562bf481
Niels van Kaam <niels@vankaam.net>
2017-07-31 13:32:44 +0200

    RecordTransformerSpec no longer depends on Kafka

--- src/test/scala/org/codefeedr/Library/RecordTransformerSpec.scala
+++ src/test/scala/org/codefeedr/Library/RecordTransformerSpec.scala
@@ -1,7 +1,7 @@
 package org.codefeedr.Library
 
 import com.typesafe.scalalogging.LazyLogging
-import org.codefeedr.Library.Internal.{RecordTransformer, RecordUtils}
+import org.codefeedr.Library.Internal.{RecordTransformer, RecordUtils, SubjectTypeFactory}
 import org.codefeedr.Model.ActionType
 import org.scalatest.{AsyncFlatSpec, BeforeAndAfterAll, Matchers}
 
@@ -19,6 +19,7 @@ class RecordTransformerSpec
     with LazyLogging
     with BeforeAndAfterAll {
 
+
   override def afterAll() {
     SubjectLibrary.UnRegisterSubject("BagTestInt")
     SubjectLibrary.UnRegisterSubject("BagTestString")
@@ -27,89 +28,69 @@ class RecordTransformerSpec
   }
 
   "A RecordTransformer" should "be able to map integers to a bag" in {
-    SubjectLibrary
-      .GetType[BagTestInt]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestInt]()
-        val obj = BagTestInt(5)
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        assert(RecordUtils.getValueT[Int]("intValue") == 5)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestInt]
+    val bagger = new RecordTransformer[BagTestInt](t)
+    val obj = BagTestInt(5)
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    assert(new RecordUtils(t).getValueT[Int]("intValue") == 5)
   }
+
   "A RecordTransformer" should "be able to map strings to a bag" in {
-    SubjectLibrary
-      .GetType[BagTestString]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestString]()
-        val obj = BagTestString("someString")
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        assert(RecordUtils.getValueT[String]("stringValue") == "someString")
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestString]
+    val bagger = new RecordTransformer[BagTestString](t)
+    val obj = BagTestString("someString")
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    assert(new RecordUtils(t).getValueT[String]("stringValue") == "someString")
   }
+
+
   "A RecordTransformer" should "be able to map any object to a bag" in {
-    SubjectLibrary
-      .GetType[BagTestObject]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestObject]()
-        val obj = BagTestObject(Seq(2, 3, 4))
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        assert(RecordUtils.getValueT[Seq[Int]]("objectValue").size == 3)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestObject]
+    val bagger = new RecordTransformer[BagTestObject](t)
+    val obj = BagTestObject(Seq(2, 3, 4))
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    assert(new RecordUtils(t).getValueT[Seq[Int]]("objectValue").size == 3)
   }
   "A RecordTransformer" should "be able to map pojo" in {
-    SubjectLibrary
-      .GetType[BagTestPojo]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestPojo]()
-        val obj = new BagTestPojo(2)
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        assert(RecordUtils.getValueT[Int]("intValue") == 2)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestPojo]
+    val bagger = new RecordTransformer[BagTestPojo](t)
+    val obj = new BagTestPojo(2)
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    assert(new RecordUtils(t).getValueT[Int]("intValue") == 2)
   }
 
   "A RecordTransformer" should "be able to map integers from a bag to object" in {
-    SubjectLibrary
-      .GetType[BagTestInt]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestInt]()
-        val obj = BagTestInt(5)
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        val r = bagger.Unbag(record)
-        assert(r.intValue == 5)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestInt]
+    val bagger = new RecordTransformer[BagTestInt](t)
+    val obj = BagTestInt(5)
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    val r = bagger.Unbag(record)
+    assert(r.intValue == 5)
   }
   "A RecordTransformer" should "be able to map strings from a bag to object" in {
-    SubjectLibrary
-      .GetType[BagTestString]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestString]()
-        val obj = BagTestString("someString")
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        val r = bagger.Unbag(record)
-        assert(r.stringValue == "someString")
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestString]
+    val bagger = new RecordTransformer[BagTestString](t)
+    val obj = BagTestString("someString")
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    val r = bagger.Unbag(record)
+    assert(r.stringValue == "someString")
   }
+
   "A RecordTransformer" should "be able to map any object from a bag to object" in {
-    SubjectLibrary
-      .GetType[BagTestObject]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestObject]()
-        val obj = BagTestObject(Seq(2, 3, 4))
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        val r = bagger.Unbag(record)
-        assert(r.objectValue.asInstanceOf[Seq[Int]].size == 3)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestObject]
+    val bagger = new RecordTransformer[BagTestObject](t)
+    val obj = BagTestObject(Seq(2, 3, 4))
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    val r = bagger.Unbag(record)
+    assert(r.objectValue.asInstanceOf[Seq[Int]].size == 3)
   }
   "A RecordTransformer" should "be able to map a bag back to pojo" in {
-    SubjectLibrary
-      .GetType[BagTestPojo]()
-      .map(t => {
-        val bagger = new RecordTransformer[BagTestPojo]()
-        val obj = new BagTestPojo(2)
-        implicit val record = bagger.Bag(obj, ActionType.Add)
-        val r = bagger.Unbag(record)
-        assert(r.intValue == 2)
-      })
+    val t = SubjectTypeFactory.getSubjectType[BagTestPojo]
+    val bagger = new RecordTransformer[BagTestPojo](t)
+    val obj = new BagTestPojo(2)
+    implicit val record = bagger.Bag(obj, ActionType.Add)
+    val r = bagger.Unbag(record)
+    assert(r.intValue == 2)
   }
 
 }
