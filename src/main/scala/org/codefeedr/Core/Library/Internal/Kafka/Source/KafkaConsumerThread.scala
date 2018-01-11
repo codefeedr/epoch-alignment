c4c782de8b2f88d493141d64899ecf30bc20ae71
Niels <niels@vankaam.net>
2018-01-11 14:14:49 +0100

    Started on moving kafkaconsumer thread to a seperate class

---
+++ src/main/scala/org/codefeedr/Core/Library/Internal/Kafka/Source/KafkaConsumerThread.scala
@@ -0,0 +1,11 @@
+package org.codefeedr.Core.Library.Internal.Kafka.Source
+
+import com.typesafe.scalalogging.LazyLogging
+
+class KafkaConsumerThread extends
+  Runnable with
+  LazyLogging{
+  override def run(): Unit = {
+
+  }
+}
