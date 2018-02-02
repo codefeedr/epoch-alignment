5abdbf2b2ac9f1a1f2ea36eeb5a3507e3daf7762
Wouter Zorgdrager <zorgdragerw@gmail.com>
2018-02-02 10:26:37 +0100

    Wrote an extra test.

--- src/test/scala/org/codefeedr/Core/Clients/GitHub/GitHubRequestServiceTest.scala
+++ src/test/scala/org/codefeedr/Core/Clients/GitHub/GitHubRequestServiceTest.scala
@@ -1,12 +1,16 @@
 package org.codefeedr.Core.Clients.GitHub
 
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.flink.calcite.shaded.com.google.common.collect.Iterables
+import org.codefeedr.Core.Clients.GitHub.GitHubProtocol.SimpleCommit
 import org.codefeedr.Core.ZkTest
 import org.eclipse.egit.github.core.client.GitHubClient
+import org.eclipse.egit.github.core.service.RepositoryService
 import org.scalatest.{AsyncFlatSpec, Matchers}
 import org.scalatest.tagobjects.Slow
 
 import scala.async.Async.{async, await}
+import collection.JavaConverters._
 
 class GitHubRequestServiceTest extends AsyncFlatSpec with Matchers with LazyLogging {
 
@@ -15,20 +19,29 @@ class GitHubRequestServiceTest extends AsyncFlatSpec with Matchers with LazyLogg
     val requestService = new GitHubRequestService(client)
 
     val commit = requestService.getCommit("codefeedr/codefeedr", "2439402a43e11b5efa2a680ac31207f2210b63d5")
-
-
     assert(commit.files.size == 5)
   }
 
-  "getAllCommits " should " should parse and return the correct commit" taggedAs (Slow) in async {
+  "getAllCommits " should " should parse and return a list of SimpleCommits" taggedAs (Slow) in async {
     val client = new GitHubClient()
-    client.setOAuth2Token("")
     val requestService = new GitHubRequestService(client)
 
-    val time = System.currentTimeMillis()
     val commits = requestService.getAllCommits("codefeedr/codefeedr")
-    println("Took " + (System.currentTimeMillis() - time) + "ms")
-    assert(1 == 1)
+    val latestCommit = commits.iterator().next().asScala.head
+
+    latestCommit should not be null
+    assert(commits.next().size() > 0)
+  }
+
+  "getAllEvents " should " should parse and return a list of Events" taggedAs (Slow) in async {
+    val client = new GitHubClient()
+    val requestService = new GitHubRequestService(client)
+
+    val events = requestService.getEvents()
+    val latestEvent = events.head
+
+    latestEvent should not be null
+    assert(events.size > 0)
   }
 
 
