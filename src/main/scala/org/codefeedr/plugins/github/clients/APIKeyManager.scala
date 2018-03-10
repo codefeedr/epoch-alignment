1a08e4f9cf28677c01456f96730b3ae6fa7f8cb3
Wouter Zorgdrager <zorgdragerw@gmail.com>
2018-03-10 22:09:06 +0100

    Started on management for API keys in ZooKeeper.

--- src/main/scala/org/codefeedr/plugins/github/clients/APIKeyManager.scala
+++ src/main/scala/org/codefeedr/plugins/github/clients/APIKeyManager.scala
@@ -20,30 +20,148 @@ package org.codefeedr.plugins.github.clients
 
 import com.typesafe.config._
 import org.codefeedr.core.library.LibraryServices
+import org.codefeedr.core.library.internal.zookeeper.ZkNode
+
 import async.Async._
 import scala.concurrent._
 import ExecutionContext.Implicits.global
-
 import scala.collection.JavaConverters._
+import resource._
 
+import scala.concurrent.duration.Duration
+
+/**
+  * Case class that represents an API key.
+  * @param key the actual key.
+  * @param requestLimit the request limit on this key.
+  * @param requestsLeft the requests left on this key.
+  * @param resetTime the time on which the request limit should be reset.
+  * @param available if the key is available to use or not.
+  */
+case class APIKey(val key: String,
+                  val requestLimit: Int,
+                  val requestsLeft: Int,
+                  var resetTime: Long = 0,
+                  available: Boolean = true)
+
+/**
+  * Manages all API keys in ZooKeeper.
+  */
 class APIKeyManager {
 
   private lazy val config: Config = ConfigFactory.load()
-
   private lazy val zkClient = LibraryServices.zkClient
 
-  lazy val keys: List[APIKey] = loadKeys()
+  //node under which the keys are stored
+  private lazy val keysNode = new KeysNode
 
+  /**
+    * Loads all the keys from the configuration.
+    * @return a list of API keys.
+    */
   def loadKeys(): List[APIKey] = {
     val apiList = config
       .getObjectList("codefeedr.input.github.keys")
       .asScala
-      .map(x => x.toConfig)
-      .map(x => new APIKey(x.getString("key"), x.getInt("limit"), System.currentTimeMillis()))
+      .map(
+        x =>
+          new APIKey(x.toConfig.getString("key"),
+                     x.toConfig.getInt("limit"),
+                     x.toConfig.getInt("limit")))
 
     apiList.toList
   }
 
-  def saveToZK() = async {}
+  /**
+    * Saves all keys from the configuration to ZooKeeper.
+    * @return all created keys.
+    */
+  def saveToZK(): Future[List[APIKey]] = async {
+    val exists = await(keysNode.exists())
+
+    //create if it doesn't exist yet
+    if (!exists) {
+      await(keysNode.create())
+    }
+
+    //get or create key
+    val createKeys = Future.sequence(loadKeys().map { x =>
+      val node = new ZkNode[APIKey](x.key, keysNode)
+      node.getOrCreate(() => x)
+    })
+
+    //await till done
+    await(createKeys)
+  }
+
+  /**
+    * Gets an available key with the most requests left.
+    * If no key is available None will be returned.
+    * @return a key or None if no key available.
+    */
+  def getKey(): Future[Option[APIKey]] = async {
+    var keyFound = false //key is not found yet
+
+    val keyNodes = await(getKeyNodes()) //get all key nodes
+    val keyData = await(Future.sequence(keyNodes.map(x => x.getData()))) //get all key data
+    var keysAvailable = keyData
+      .map(_.get)
+      .filter(_.available)
+      . //check if available
+      sortWith(_.requestsLeft > _.requestsLeft) //sort on highest requests left
+
+    //set currentKey to None
+    var returnKey: Option[APIKey] = None
+
+    //keep looking for an available key until none is left
+    while (!keyFound && keysAvailable.size > 0) {
+      val firstKey = keysAvailable.head //get key with most requests left
+
+      //try to acquire this key by looking it
+      val acquireKey = await(checkKey(new ZkNode[APIKey](s"${firstKey.key}", keysNode)))
+      keyFound = acquireKey._1 //will be true if the key is successfully acquired
+      keysAvailable = keysAvailable.filter(_.key != firstKey.key) //remove it from the list
+
+      //set returnKey if it is found
+      returnKey = if (keyFound) Some(acquireKey._2) else None
+    }
+
+    //return key if found (or not).
+    returnKey
+  }
+
+  /**
+    * Get all keynodes under the /Keys path.
+    * @return all ZkNodes found.
+    */
+  private def getKeyNodes(): Future[List[ZkNode[APIKey]]] = async {
+    //get all children
+    val children = await(zkClient.GetChildren(keysNode.path())).toList
+
+    //get all keys
+    children.map(key => new ZkNode[APIKey](s"$key", keysNode))
+  }
+
+  /**
+    * Tries to check and acquire key using a WriteLock.
+    * @param key the key to acquire.
+    * @return (true, key) if the key is successfully locked, (false, key) if not.
+    */
+  private def checkKey(key: ZkNode[APIKey]): Future[(Boolean, APIKey)] = async {
+    val lock = await(key.writeLock()) //await the write lock
+
+    managed(lock).acquireAndGet { x =>
+      var data: APIKey = Await.result(key.getData(), Duration.Inf).get //get the key
+
+      if (data.available) { //if available
+        data = data.copy(available = false) //set unavailable
+        Await.result(key.setData(data), Duration.Inf)
+        (true, data)
+      } else {
+        (false, data)
+      }
+    }
+
+  }
 
 }
---
+++ src/test/scala/org/codefeedr/plugins/github/clients/APIKeyManagerTest.scala
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.codefeedr.plugins.github.clients
+
+import org.codefeedr.core.LibraryServiceSpec
+import org.codefeedr.core.library.internal.zookeeper.{ZkNode, ZkNodeBase}
+import org.scalatest.BeforeAndAfterEach
+
+import scala.async.Async._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class APIKeyManagerTest extends LibraryServiceSpec  with BeforeAndAfterEach {
+
+  val keyNode = new KeysNode
+
+
+  override def beforeEach() : Unit = {
+    Await.ready(keyNode.deleteRecursive(), Duration(1, SECONDS))
+  }
+
+  "The APIKeyManager" should "correctly store all keys in ZooKeeper" in async {
+    val keyManager = new APIKeyManager()
+
+    await(keyManager.saveToZK())
+    val children = await(zkClient.GetChildren("/keys")).toList
+
+    assert(children.size == keyManager.loadKeys().size)
+    assert(children == keyManager.loadKeys().map(_.key).reverse)
+  }
+
+  "The APIKeyManager" should "lock a key when a key is requested" in async {
+    val keyManager = new APIKeyManager()
+    await(keyManager.saveToZK())
+
+    val keyLocked = await(keyManager.getKey())
+
+    val children = await(zkClient.GetChildren("/keys")).toList
+    val childrenData = await(Future.sequence(children.map { x =>
+      val node = new ZkNode[APIKey](x, keyNode)
+      node.getData()
+    }))
+
+    val foundKey = childrenData.find(x => x.get.key == keyLocked.get.key).get
+    assert(foundKey.get.available == false) //foundKey should be locked
+  }
+
+  "The APIKeyManager" should "lock multiple keys when multiple requests are done" in async {
+    val keyManager = new APIKeyManager()
+    await(keyManager.saveToZK())
+
+    val calls = keyManager.getKey() :: keyManager.getKey() :: keyManager.getKey() :: Nil
+    val keysLocked = await(Future.sequence(calls))
+
+    val children = await(zkClient.GetChildren("/keys")).toList
+    val childrenData = await(Future.sequence(children.map { x =>
+      val node = new ZkNode[APIKey](x, keyNode)
+      node.getData()
+    }))
+
+    assert(childrenData.filter(!_.get.available).size == 2) //2 keys are locked
+    assert(keysLocked.filter(_.isEmpty).size == 1) //1 getKey called returned None
+    assert(keysLocked.filter(!_.isEmpty).size == 2) //2 getKeys called returned an actual key
+  }
+
+
+
+
+}
