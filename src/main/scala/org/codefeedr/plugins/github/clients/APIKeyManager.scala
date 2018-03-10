c4947de797aa76d3dfe5aace56cf7a15c9662525
Wouter Zorgdrager <zorgdragerw@gmail.com>
2018-03-10 14:05:36 +0100

    Started on a key manager.

---
+++ src/main/scala/org/codefeedr/plugins/github/clients/APIKeyManager.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.codefeedr.plugins.github.clients
+
+import com.typesafe.config._
+import org.codefeedr.core.library.LibraryServices
+import async.Async._
+
+import scala.collection.JavaConverters._
+
+class APIKeyManager {
+
+  private lazy val config: Config = ConfigFactory.load()
+
+  private lazy val zkClient = LibraryServices.zkClient
+
+  lazy val keys: List[APIKey] = loadKeys()
+
+  def loadKeys(): List[APIKey] = {
+    val apiList = config
+      .getObjectList("codefeedr.input.github.keys")
+      .asScala
+      .map(x => x.toConfig)
+      .map(x => new APIKey(x.getString("key"), x.getInt("limit"), System.currentTimeMillis()))
+
+    apiList.toList
+  }
+
+  def saveToZK() = async {
+
+  }
+
+}
