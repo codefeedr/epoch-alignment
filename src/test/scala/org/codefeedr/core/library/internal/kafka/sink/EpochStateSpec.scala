2da7ff29b17c3d85fb7696aa21b632db42ee5c60
Niels <niels@vankaam.net>
2018-04-09 11:10:44 +0200

    Added test for commit()

--- src/test/scala/org/codefeedr/core/library/internal/kafka/sink/EpochStateSpec.scala
+++ src/test/scala/org/codefeedr/core/library/internal/kafka/sink/EpochStateSpec.scala
@@ -53,6 +53,8 @@ class EpochStateSpec extends AsyncFlatSpec with MockitoSugar with BeforeAndAfter
     verify(p2).create(Partition(2,12l))
     verify(epochNode, never()).create()
     verify(epochPartitions, never()).create()
+    verify(p1,never()).setState(true)
+    verify(p2, never()).setState(true)
 
     assert(true)
   }
@@ -86,8 +88,41 @@ class EpochStateSpec extends AsyncFlatSpec with MockitoSugar with BeforeAndAfter
     verify(p2).create(Partition(2,12l))
     verify(epochNode).create()
     verify(epochPartitions).create()
+    verify(p1,never()).setState(true)
+    verify(p2, never()).setState(true)
+
     assert(true)
   }
+
+
+  "EpochState.Commit()" should "Set all EpochPartitions to true" in async {
+    //Arrange
+    val transactionState = new TransactionState(0,10,0,false,mutable.Map(("",1) -> 10L, ("",2) -> 12L))
+    val p1 = mock[EpochPartition]
+    val p2 = mock[EpochPartition]
+    when(epochPartitions.getChild("1")) thenReturn p1
+    when(epochPartitions.getChild("2")) thenReturn p2
+
+
+    when(p1.create(Partition(1,10l))) thenReturn Future.successful(Partition(1,10l))
+    when(p2.create(Partition(2,12l))) thenReturn Future.successful(Partition(2,12l))
+    when(p1.setState(true)) thenReturn Future.successful()
+    when(p2.setState(true)) thenReturn Future.successful()
+
+    val EpochState = new EpochState(transactionState,epochCollectionNode)
+
+    //Act
+    await(EpochState.preCommit())
+    await(EpochState.commit())
+
+    //Assert
+    verify(p1, times(1)).setState(true)
+    verify(p2, times(1)).setState(true)
+
+    assert(true)
+  }
+
+
 
   def answer[T](f: InvocationOnMock => T): Answer[T] = {
     //Ignore the warning, compiler needs it
