11977cfb1c0a9d4dbe51a94ee73c58bc5b5eb7b6
Niels van Kaam <niels@vankaam.net>
2018-10-05 14:06:47 +0200

    Added a few more utility scripts

--- scripts/util/kafkastats.sh
+++ scripts/util/kafkastats.sh
@@ -22,3 +22,12 @@ kafka-topics.sh --list --zookeeper "$ZOOKEEPER"
 
 #Delete all topics created by codefeedr
 kafka-topics.sh --zookeeper "$ZOOKEEPER" --delete --topic codefeedr.*
+
+
+#Topic info
+kafka-topics.sh  --zookeeper "$ZOOKEEPER" --describe --topic codefeedr.*
+
+
+# Messages in topic
+TOPIC="codefeedr_HotIssue_ae327cf4-0006-47ab-abb2-a35986c87e6b"
+kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list localhost:9092 --topic $TOPIC --time -1 --offsets 1 | awk -F ":" '{sum += $3} END {print sum}'
