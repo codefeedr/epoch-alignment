209eadf6c40b4ab2576633fdeb379ef4232266fa
Niels van Kaam <niels@vankaam.net>
2018-10-09 13:44:51 +0200

    Attempt to fix kafka and zk deployment

--- scripts/deployment/kafka_elk_athens_install.sh
+++ scripts/deployment/kafka_elk_athens_install.sh
@@ -2,4 +2,4 @@
 
 #Docker version: Docker version 18.03.1-ce
 
-ssh nvankaam@athens.ewi.tudelft.nl  'bash -s' < scripts/install_kafka_elk.sh
+ssh nvankaam@131.180.119.50  'bash -s' < scripts/install_kafka_elk.sh
