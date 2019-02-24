8045f0e2697f9c1594ca7b4311a43d57b186c0dd
Niels van Kaam <niels@vankaam.net>
2019-02-24 03:48:40 +0100

    .....

--- scripts/deployment/deployment/experiment1/deployHotPullrequestKafka.sh
+++ scripts/deployment/deployment/experiment1/deployHotPullrequestKafka.sh
@@ -2,8 +2,8 @@
 # Need to deploy the job publishing hot issues to kafka
 
 
-#bash ../shared/clearZookeeper.sh
-#sleep 10
+bash ../shared/clearZookeeper.sh
+
 bash deployHotIssueQueryKafkaSink.sh $1
 sleep 1
 bash deployHotPullrequestKafkaSource.sh $1
